4bee48740e110e05558eea7afa1b691a2adbd742
Jake Cattrall <krazyjakee@gmail.com>
2014-05-14 15:56:36 +0100

    idea is in place, now for testing

--- keyframes.spritesheet.js
+++ keyframes.spritesheet.js
@@ -1,7 +1,11 @@
 
 (function() {
 
+  vendorPrefix + vendorPrefix = $.keyframe.getVendorPrefix();
+
   $.keyframe = $.extend($.keyframe, {
+
+    spriteSheets: {},
 
     spriteSheet: function(opts){
 
@@ -16,25 +20,59 @@
         count: (rows * cols),
         spriteWidth: (opts.width / opts.cols),
         spriteHeight: (opts.height / opts.rows),
-        fps: 24,
         loop: true
       };
 
       $.extend(defaults, opts);
 
+      $.keyframe.spriteSheets[opts.name] = opts;
+
+      spriteStep = 100 / opts.count;
       spriteFrames = {};
-      var x = 0;
-      var y = 0;
+      var x = opts.offsetX;
+      var y = opts.offsetY;
       for(var i = 0; i < opts.count; i++){
-        
+        spriteFrames[(spriteStep * i) + '%'] = {
+          'background-position': '-' + (opts.spriteWidth + x) + 'px -' + (opts.spriteHeight + y) + 'px'
+        }
+        if(x >= (opts.cols * opts.spriteWidth)){
+          y += opts.spriteHeight;
+          x = 0;
+        }else{
+          x += opts.spriteWidth;
+        }
       }
 
-      $.keyframe.define([
-        $.extend({ name: opts.name }, spriteFrames)
-      ]);
+      return $.extend({ name: opts.name }, spriteFrames);
 
     }
 
   });
+
+  $.fn.playSpriteSheet = function(name, time, loops, opts){
+    if(opts){
+      opts['name'] = name;
+      $.keyframe.spriteSheet(opts);
+      $(this).playSpriteSheet(name);
+    }else{
+      opts = $.keyframe.spriteSheets[name];
+      if(opts){
+        if(loops){
+          if(loops < 0){
+            loops = 'infinite';
+          }
+        }else{
+          loops = 'infinite';
+        }
+
+        var animate = name + ' ' + time + ' steps(' + opts.count + ') ' + loops;
+        var existingAnimation = $(this).css('animation');
+        if(existingAnimation){
+          animate = existingAnimation + ', ' + animate;
+        }
+        $(this).css(vendorPrefix + 'animation', animate);
+      }
+    }
+  }
 
 }).call(this);
